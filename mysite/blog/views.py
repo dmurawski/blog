ef4ab6fea84ad1fe243f5ab948544f2a4200bcd2
Damian Murawski <dmurawski018@gmail.com>
2024-12-06 00:32:59 +0100

    add form comment and add view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,8 +1,9 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_POST
 from .models import Post
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 
 
 def post_share(request, post_id):
@@ -68,3 +69,33 @@ def post_detail(request, year, month, day, post):
         publish__day=day,
     )
     return render(request, "blog/post/detail.html", {"post": post})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED,
+    )
+
+    comment = False
+
+    form = CommentForm(data=request.POST)
+
+    if form.is_valid():
+        # Stworzenie komentarza bez zapisywania do bazy danych
+        comment = form.save(commit=False)
+        # przypisanie posta do komentarza
+        comment.post = post
+        comment.save()
+
+    return render(
+        request,
+        "blog/post/comment.html",
+        {
+            "post": post,
+            "comment": comment,
+            "form": form,
+        },
+    )
