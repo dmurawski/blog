c365ef0f2aab98061c83adce067781272226e20b
Damian Murawski <dmurawski018@gmail.com>
2024-12-05 23:08:51 +0100

    add SEO-friendly URLs for posts

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -15,7 +15,10 @@ class Post(models.Model):
         PUBLISHED = "PB", "Published"
 
     title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(
+        max_length=250,
+        unique_for_date="publish",
+    )
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
@@ -43,4 +46,12 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse("blog:post_detail", args=[self.id])
+        return reverse(
+            "blog:post_detail",
+            args=[
+                self.publish.year,
+                self.publish.month,
+                self.publish.day,
+                self.slug,
+            ],
+        )
