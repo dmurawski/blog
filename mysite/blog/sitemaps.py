7e54cbe2ea8b25270ec83ff1b5a84c71c04e15a7
Damian Murawski <dmurawski018@gmail.com>
2024-12-07 00:31:42 +0100

    extend sitemap with functionality to include url to each tag in the blog

--- mysite/blog/sitemaps.py
+++ mysite/blog/sitemaps.py
@@ -1,5 +1,19 @@
 from django.contrib.sitemaps import Sitemap
 from .models import Post
+from taggit.models import Tag
+from django.urls import reverse
+
+
+class TagSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.8
+
+    def items(self):
+        return Tag.objects.all()
+
+    def location(self, obj):
+
+        return reverse("blog:post_list_by_tag", kwargs={"tag_slug": obj.slug})
 
 
 class PostSitemap(Sitemap):
