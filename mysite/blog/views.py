27dc9f4598ff8615c6b9346bf6733f4fea29025f
Damian Murawski <dmurawski018@gmail.com>
2024-12-07 00:01:39 +0100

    add search posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,9 +3,10 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
 from taggit.models import Tag
 
-from .forms import CommentForm, EmailPostForm
+from .forms import CommentForm, EmailPostForm, SearchForm
 from .models import Post
 
 
@@ -135,5 +136,29 @@ def post_comment(request, post_id):
             "post": post,
             "comment": comment,
             "form": form,
+        },
+    )
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if "query" in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data["query"]
+            results = Post.published.annotate(
+                search=SearchVector("title", "body"),
+            ).filter(search=query)
+
+    return render(
+        request,
+        "blog/post/search.html",
+        {
+            "form": form,
+            "query": query,
+            "results": results,
         },
     )
